610a71b5180c516f2b41bf8ba7983d2499c64fdf
Chriton <defisummer@gmail.com>
2023-04-04 01:06:26 +0300

    fixed hover color for github link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,10 +73,15 @@ const config = {
           {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
-            position: 'left',
+            position: 'right',
             label: 'Docs',
           },
           // {to: '/blog', label: 'Blog', position: 'left'},
+          // {
+          //   to: 'https://linagee.app',
+          //   position: 'right',
+          //   label: 'App',
+          // },
           {
             href: 'https://github.com/Linagee-Name-Registrar',
             // label: 'GitHub',
@@ -134,6 +139,38 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      // algolia: {
+      //
+      //   className: 'algolia-search',
+      //
+      //   // The application ID provided by Algolia
+      //   appId: 'YOUR_APP_ID',
+      //
+      //   // Public API key: it is safe to commit it
+      //   apiKey: 'YOUR_SEARCH_API_KEY',
+      //
+      //   indexName: 'YOUR_INDEX_NAME',
+      //
+      //   // Optional: see doc section below
+      //   contextualSearch: true,
+      //
+      //   // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      //   externalUrlRegex: 'external\\.com|domain\\.com',
+      //
+      //   // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      //   replaceSearchResultPathname: {
+      //     from: '/docs/', // or as RegExp: /\/docs\//
+      //     to: '/',
+      //   },
+      //
+      //   // Optional: Algolia search parameters
+      //   searchParameters: {},
+      //
+      //   // Optional: path for search page that enabled by default (`false` to disable it)
+      //   searchPagePath: 'search',
+      //
+      //   //... other Algolia params
+      // },
     }),
 };
 
